b53a3a76f52079c4dc05857b18d9ff72eed7c303
Charlton Ortega <charltonortega14@gmail.com>
2023-08-26 23:35:06 +0930

    enhanced clog middleware

--- middleware/clog.js
+++ middleware/clog.js
@@ -1,6 +1,26 @@
 const clog = (req, res, next) => {
-    console.log(`[${new Date().toUTCString()}] ${req.method} ${req.path}`);
+    const date = new Date();
+  
+    let methodColor = '\x1b[37m'; // (white)
+    let successMessage = '';
+  
+    switch (req.method) {
+      case 'GET':
+        methodColor = '\x1b[32m'; // Green
+        break;
+      case 'POST':
+        methodColor = '\x1b[34m'; // Blue
+        successMessage = 'POST request successful';
+        break;
+      default:
+        break;
+    }
+  
+    console.log(
+      `[${date.toLocaleString()}] ${methodColor}${req.method}\x1b[0m ${req.url} ${successMessage}` // log time, method, url, and success
+    );
+  
     next();
-};
-
-module.exports = clog;
+  };
+  
+  module.exports = clog;
--- server.js
+++ server.js
@@ -7,11 +7,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(clog);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-
+app.use(clog);
 // HTML Routes
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
